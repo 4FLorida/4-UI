334354c9327b1c18efba7a3fd5eb25555c53aa84
Berkay Burak <Exper1ment4L@users.noreply.github.com>
2019-12-06 14:00:05 +0300

    Alert edit

--- src/App.js
+++ src/App.js
@@ -83,16 +83,16 @@ function App() {
           <TextInput type="password"></TextInput>
         </Col>
       </Row>
-      <Row>
+      <Row center>
         <h1>Alerts</h1>
       </Row>
       <Row>
-        <Col md="5">
-         <Alert blue >This is an alert!!check it out! </Alert>
-         <Alert yellow >This is an alert!!check it out!</Alert>
-         <Alert red >This is an alert!!check it out! </Alert>
-         <Alert dark >  This is an alert!!check it out! </Alert>
-         <Alert light > This is an alert!!check it out! </Alert>
+        <Col>
+          <Alert blue>This is an alert!!check it out! </Alert>
+          <Alert yellow>This is an alert!!check it out!</Alert>
+          <Alert red>This is an alert!!check it out! </Alert>
+          <Alert dark> This is an alert!!check it out! </Alert>
+          <Alert light> This is an alert!!check it out! </Alert>
         </Col>
       </Row>
     </Container>
