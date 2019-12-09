850ca5c6b0680b8da3d9ba4e901dbe406ba479b9
Berkay Burak <Exper1ment4L@users.noreply.github.com>
2019-12-10 00:26:48 +0300

    heroku import settings

--- src/components/Col.js
+++ src/components/Col.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from '../../node_modules/styled-components';
 
 function getWidth(span) {
   if (!span) return;
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from '../../node_modules/styled-components';
 
 
 const TextInput = styled.input`
